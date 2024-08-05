070eb96a0efc3805bb0c4b0f80b73346ff45bf2b
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2024-08-05 10:59:13 +0000

    chore: Added docker in docker. feat: Added exporting insights to chart and storing it in AWS S3.

--- src/analyst/analyst.py
+++ src/analyst/analyst.py
@@ -1,9 +1,12 @@
+import io
 import json
 import pandas
 from datetime import datetime
 from dotenv import load_dotenv
 import os
 import boto3
+from matplotlib import pyplot
+from matplotlib.patches import Patch
 
 load_dotenv()
 
@@ -124,7 +127,57 @@ def get_insights() -> dict:
     return insights
 
 
-def store_insights(insights: dict):
+def get_chart_data(insights: dict) -> dict:
+    labels = ["Correct Answers"]
+    labels.extend(
+        [
+            incorrect_answers_detailed_insight["cause"]
+            for incorrect_answers_detailed_insight in insights["insights"]["incorrect"][
+                "detailed_insights"
+            ]
+        ]
+    )
+
+    sizes = [insights["insights"]["correct"]["number"]]
+    sizes.extend(
+        [
+            incorrect_answers_detailed_insight["number"]
+            for incorrect_answers_detailed_insight in insights["insights"]["incorrect"][
+                "detailed_insights"
+            ]
+        ]
+    )
+
+    return {"labels": labels, "sizes": sizes}
+
+
+def get_chart_buffer(chart_data):
+    image_buffer = io.BytesIO()
+
+    pyplot.figure(figsize=(10, 6))
+    pyplot.pie(
+        chart_data["sizes"],
+        labels=chart_data["labels"],
+        autopct="%1.1f%%",
+        startangle=140,
+    )
+    pyplot.axis("equal")
+
+    # Save the plot to the BytesIO buffer
+    pyplot.savefig(image_buffer, format="png")
+
+    # Seek to the start of the buffer
+    image_buffer.seek(0)
+
+    with open("1.png", 'wb') as f:
+        f.write(image_buffer.getvalue())
+
+    pyplot.close()
+
+    return image_buffer
+
+
+def store_insights(insights: dict, image):
     s3 = boto3.resource("s3")
 
     bucket = s3.Bucket(os.environ["S3_BUCKET_NAME"])
@@ -132,9 +185,20 @@ def store_insights(insights: dict):
     bucket.put_object(
         Key=os.path.join(
             os.environ["S3_BUCKET_FOLDER"],
-            f"results-{datetime.now().strftime('%d-%m-%Y')}",
+            datetime.now().strftime("%d-%m-%Y"),
+            f"insights.json",
         ),
         Body=json.dumps(insights),
+    )
+
+    bucket.put_object(
+        Key=os.path.join(
+            os.environ["S3_BUCKET_FOLDER"],
+            datetime.now().strftime("%d-%m-%Y"),
+            f"chart.png",
+        ),
+        Body=image,
+        ContentType="image/png",
     )
 
 
@@ -146,6 +210,10 @@ def handler(event, context):
     if not dump_results or dump_results == "False":
         return insights
 
-    store_insights(insights)
+    chart_data = get_chart_data(insights)
+
+    chart_buffer = get_chart_buffer(chart_data)
+
+    store_insights(insights, chart_buffer)
 
     return insights
